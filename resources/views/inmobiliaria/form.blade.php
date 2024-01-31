14dadefe582c1d689f8e55554bcfb8b790cf167a
Montserrat <tmontse101@gmail.com>
2024-01-31 11:52:38 -0600

    actualizacion de formato de formularios

--- resources/views/inmobiliaria/form.blade.php
+++ resources/views/inmobiliaria/form.blade.php
@@ -1,23 +1,31 @@
 <h1> {{ $modo }} inmobiliaria </h1>
-<label for="id">ID:</label>
-<input type="text" name="Id" value="{{isset($inmobiliaria->id) ?$inmobiliaria->id: '' }}"  id="id">
+
+<div class="form-group">
+    <label for="id">ID:</label>
+    <input type="text" name="Id" class="form-control" value="{{isset($inmobiliaria->id) ?$inmobiliaria->id: '' }}"  id="id">
+</div>
+
+<div class="form-group">
+    <label for="nombre">Nombre:</label>
+    <input type="text" name="Nombre" class="form-control" value="{{isset($inmobiliaria->Nombre) ?$inmobiliaria->Nombre: '' }}" id="Nombre">
+</div>
+
+<div class="form-group">
+    <label for="rfc">RFC:</label>
+    <input type="text" name="RFC" class="form-control" value="{{ isset($inmobiliaria->RFC) ?$inmobiliaria->RFC: '' }}" id="RFC">
+    
+</div>
+
+<div class="form-group">
+    <label for="estado">Estado:</label>
+
+    <select name="estado" class="form-control" value="{{ isset($inmobiliaria->estado) ?$inmobiliaria->estado: '' }}" id="estado">
+        <option value="activo">Activo</option>
+        <option value="inactivo">Inactivo</option>
+    </select>
 <br>
+</div>
 
-<label for="nombre">Nombre:</label>
-<input type="text" name="Nombre" value="{{isset($inmobiliaria->Nombre) ?$inmobiliaria->Nombre: '' }}" id="Nombre">
-<br>
+    <a class="btn btn-primary" href="{{ url('inmobiliaria/') }}">Regresar</a>
 
-<label for="rfc">RFC:</label>
-<input type="text" name="RFC" value="{{ isset($inmobiliaria->RFC) ?$inmobiliaria->RFC: '' }}" id="RFC">
-<br>
-
-<label for="estado">Estado:</label>
-
-<select name="estado" value="{{ isset($inmobiliaria->estado) ?$inmobiliaria->estado: '' }}" id="estado">
-    <option value="activo">Activo</option>
-    <option value="inactivo">Inactivo</option>
-</select>
-<br>
-<a href="{{ url('inmobiliaria/') }}">Regresar</a>
-
-<input type="submit" value="{{ $modo }} datos">
+    <input class="btn btn-success" type="submit" value="{{ $modo }} datos">
